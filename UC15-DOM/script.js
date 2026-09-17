bce488ed0bb8471843b4319e7e6cd8d8e0d8dfd3
juan-67 <xstone776@gmail.com>
2026-09-17 17:52:21 -0300

    Add files via upload

---
+++ UC15-DOM/script.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    document.getElementById("titulo").textContent = 'você clicou no botão!';
+    
+}
+
+function mudarCor() {
+    document.getElementByld("mensagem").style.color = 'blue';
+}
